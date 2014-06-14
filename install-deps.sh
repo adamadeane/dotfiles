d990102cae3a0689cf8f74d0c97b1558b206a20d
Adam Adeane <adam@adeane.co.nz>
2014-06-15 00:38:46 +1200

    Add RVM

--- install-deps.sh
+++ install-deps.sh
@@ -9,3 +9,6 @@ git clone git://github.com/jamiew/git-friendly.git ~/dev/git-friendly
 
 # Homebrew
 ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go/install)
+
+# https://rvm.io
+curl -L https://get.rvm.io | bash -s stable --ruby
