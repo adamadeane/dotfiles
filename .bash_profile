a4fd26f4df978b801c1d80ac3be27574f4588ff6
Adam Adeane <adam@adeane.co.nz>
2014-06-14 19:11:16 +1200

    Add z, Homebrew and git-friendly

--- .bash_profile
+++ .bash_profile
@@ -46,3 +46,6 @@ complete -W "NSGlobalDomain" defaults;
 
 # Add `killall` tab completion for common apps
 complete -o "nospace" -W "Contacts Calendar Dock Finder Mail Safari iTunes SystemUIServer Terminal Twitter" killall;
+
+# init z https://github.com/rupa/z
+. ~/z/z.sh
---
+++ install-deps.sh
@@ -0,0 +1,10 @@
+# https://github.com/rupa/z
+cd
+git clone https://github.com/rupa/z.git
+chmod +x ~/z/z.sh
+
+# Homebrew
+ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go/install)
+
+# https://github.com/jamiew/git-friendly
+bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
